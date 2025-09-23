b4ee389100575c6ec52f2547a6386f2adf737ad3
yoyaccio <yoyaccio@gmail.com>
2025-09-23 19:37:19 +0200

    FIX: register Player settings page for MellowDSP + update SHA

--- Plugins/MellowDSP/Plugin.pm
+++ Plugins/MellowDSP/Plugin.pm
@@ -5,6 +5,7 @@ use warnings;
 use Slim::Utils::Log;
 use Slim::Utils::Prefs;
 use Slim::Web::Pages;
+use Slim::Web::Settings;
 
 use Plugins::MellowDSP::PlayerSettings;
 
@@ -14,9 +15,17 @@ my $prefs = preferences('plugin.mellowdsp');
 sub initPlugin {
     $log->info("Initializing MellowDSP plugin");
 
+    # Registriamo la pagina web
     Slim::Web::Pages->addPageFunction(
         'plugins/MellowDSP/settings/basic.html',
         \&Plugins::MellowDSP::PlayerSettings::settingsPage
+    );
+
+    # Registriamo la scheda Player
+    Slim::Web::Settings::Player::registerSettingsPage(
+        'MellowDSP',
+        \&Plugins::MellowDSP::PlayerSettings::settingsPage,
+        'PLUGIN_MELLOWDSP'
     );
 
     return 1;
