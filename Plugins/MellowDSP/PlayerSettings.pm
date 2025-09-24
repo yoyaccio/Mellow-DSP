cb9aff755f2605948268ebae6d4bbd9722590d6f
yoyaccio <yoyaccio@gmail.com>
2025-09-24 21:05:28 +0200

    Release 0.1.20 - add per-player settings UI

--- Plugins/MellowDSP/PlayerSettings.pm
+++ Plugins/MellowDSP/PlayerSettings.pm
@@ -1,53 +1,26 @@
 package Plugins::MellowDSP::PlayerSettings;
+
 use strict;
 use warnings;
-use Slim::Utils::Log;
 use Slim::Utils::Prefs;
-use Slim::Web::HTTP;
-use Slim::Web::Pages;
+use Slim::Utils::Log;
 
-my $log   = logger('plugin.mellowdsp');
 my $prefs = preferences('plugin.mellowdsp');
+my $log   = logger('plugin.mellowdsp');
 
-sub init {
-    Slim::Web::Pages->addPageFunction(
-        'plugins/MellowDSP/settings/player.html',
-        \&settingsPage
-    );
-    return 1;
-}
-
-sub settingsPage {
-    my ($client, $params, $cb, @args) = @_;
-    my $c = $prefs->client($client);
+sub handler {
+    my ($class, $client, $params, $callback, @args) = @_;
 
     if ($params->{saveSettings}) {
-        $c->set('enabled',          $params->{enabled} ? 1 : 0);
-        $c->set('upsample_rate',    $params->{upsample_rate}    || '44100');
-        $c->set('phase_response',   $params->{phase_response}   || 'linear');
-        $c->set('output_depth',     $params->{output_depth}     || '24');
-        $c->set('dithering',        $params->{dithering}        || 'none');
-        $c->set('target_precision', $params->{target_precision} || '24');
-        $c->set('output_format',    $params->{output_format}    || 'wav');
-        $c->set('mode',             $params->{mode}             || 'server_dac_wav');
-        $c->set('fir_file_left',    $params->{fir_file_left}    || '');
-        $c->set('fir_file_right',   $params->{fir_file_right}   || '');
+        $prefs->set('enabled', $params->{enabled} ? 1 : 0);
+        $prefs->set('upsample_rate', $params->{upsample_rate} || '44100');
     }
 
-    $params->{enabled}          = $c->get('enabled')          || 0;
-    $params->{upsample_rate}    = $c->get('upsample_rate')    || '44100';
-    $params->{phase_response}   = $c->get('phase_response')   || 'linear';
-    $params->{output_depth}     = $c->get('output_depth')     || '24';
-    $params->{dithering}        = $c->get('dithering')        || 'none';
-    $params->{target_precision} = $c->get('target_precision') || '24';
-    $params->{output_format}    = $c->get('output_format')    || 'wav';
-    $params->{mode}             = $c->get('mode')             || 'server_dac_wav';
-    $params->{fir_file_left}    = $c->get('fir_file_left')    || '';
-    $params->{fir_file_right}   = $c->get('fir_file_right')   || '';
-    $params->{player}           = $client ? $client->id() : '';
+    $params->{enabled}       = $prefs->get('enabled')       || 0;
+    $params->{upsample_rate} = $prefs->get('upsample_rate') || '44100';
 
     return Slim::Web::HTTP::filltemplatefile(
-        'plugins/MellowDSP/settings/player.html',
+        'plugins/MellowDSP/settings/basic.html',
         $params
     );
 }
--- Plugins/MellowDSP/Plugin.pm
+++ Plugins/MellowDSP/Plugin.pm
@@ -1,18 +1,33 @@
 package Plugins::MellowDSP::Plugin;
+
 use strict;
 use warnings;
+
 use Slim::Utils::Log;
 use Slim::Utils::Prefs;
+use Slim::Web::Settings;
+
 use Plugins::MellowDSP::PlayerSettings;
 
 my $log   = logger('plugin.mellowdsp');
 my $prefs = preferences('plugin.mellowdsp');
 
 sub initPlugin {
-    Plugins::MellowDSP::PlayerSettings->init();
+    $log->info("Inizializzazione MellowDSP...");
+
+    Slim::Web::Settings->addSettingsPage(
+        {
+            'id'     => 'MellowDSP',
+            'name'   => 'PLUGIN_MELLOWDSP',
+            'module' => 'Plugins::MellowDSP::PlayerSettings',
+        }
+    );
+
     return 1;
 }
 
-sub getDisplayName { 'PLUGIN_MELLOWDSP' }
+sub getDisplayName {
+    return 'PLUGIN_MELLOWDSP';
+}
 
 1;
