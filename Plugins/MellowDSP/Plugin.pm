2fb19f8c2c376fe53376d61035783c1139ad62ec
yoyaccio <yoyaccio@gmail.com>
2025-09-25 00:16:18 +0200

    MellowDSP 0.2.2: rebuild plugin with correct strings and title

--- Plugins/MellowDSP/Plugin.pm
+++ Plugins/MellowDSP/Plugin.pm
@@ -5,26 +5,21 @@ use warnings;
 use Slim::Utils::Log;
 use Slim::Utils::Prefs;
 
-# categoria log
 my $log = Slim::Utils::Log->addLogCategory({
   category     => 'plugin.mellowdsp',
   defaultLevel => 'ERROR',
   description  => 'PLUGIN_MELLOWDSP',
 });
 
-# prefs del plugin
 my $prefs = preferences('plugin.mellowdsp');
 
 sub initPlugin {
-  # default prefs
   $prefs->init({ enabled => 0 });
 
-  # registra pagina Settings solo se c'è la WebUI
   if ( main::WEBUI ) {
     require Plugins::MellowDSP::Settings;
     Plugins::MellowDSP::Settings->new;
   }
-
   return 1;
 }
 
--- Plugins/MellowDSP/Settings.pm
+++ Plugins/MellowDSP/Settings.pm
@@ -9,25 +9,21 @@ use Slim::Utils::Log;
 my $log   = logger('plugin.mellowdsp');
 my $prefs = preferences('plugin.mellowdsp');
 
-# titolo mostrato nella UI (usa strings.txt)
 sub name        { return 'PLUGIN_MELLOWDSP'; }
-# template TT2 da rendere
 sub page        { return 'plugins/MellowDSP/settings/basic.html'; }
-# prefs radice del plugin
 sub prefs       { return $prefs; }
-# richiede selezione player -> appare sotto Settings → Player
 sub needsClient { return 1; }
 
 sub handler {
-  my ($class, $client, $params) = @_;
+    my ($class, $client, $params) = @_;
 
-  if ($params->{saveSettings}) {
-    $prefs->client($client)->set('enabled', $params->{enabled} ? 1 : 0);
-  }
+    if ($params->{saveSettings}) {
+        $prefs->client($client)->set('enabled', $params->{enabled} ? 1 : 0);
+    }
 
-  $params->{enabled} = $prefs->client($client)->get('enabled') || 0;
+    $params->{enabled} = $prefs->client($client)->get('enabled') || 0;
 
-  return $class->SUPER::handler($client, $params);
+    return $class->SUPER::handler($client, $params);
 }
 
 1;
