e2bcea6bb13479a7a591db2e1bf7ad628071f403
yoyaccio <yoyaccio@gmail.com>
2025-09-27 03:00:05 +0200

    Ultra minimal working plugin v0.3.15
    - Remove Settings.pm that was causing plugin deactivation
    - Keep only basic Plugin.pm with minimal functionality
    - Focus on getting plugin to stay active first
    - Will add UI functionality once base plugin is stable

--- Plugins/MellowDSP/Plugin.pm
+++ Plugins/MellowDSP/Plugin.pm
@@ -5,7 +5,6 @@ use warnings;
 use base qw(Slim::Plugin::Base);
 use Slim::Utils::Log;
 use Slim::Utils::Prefs;
-use Plugins::MellowDSP::Settings;
 
 my $log   = logger('plugin.mellowdsp');
 my $prefs = preferences('plugin.mellowdsp');
@@ -14,20 +13,10 @@ sub initPlugin {
     my $class = shift;
     $log->info("MellowDSP plugin initialized");
     $class->SUPER::initPlugin(@_);
-    
-    $prefs->init({
-        enabled => 0,
-        upsampling => '44100',
-        phase => 'linear',
-    });
 }
 
 sub getDisplayName {
     return 'PLUGIN_MELLOWDSP';
-}
-
-sub settingsClass {
-    return 'Plugins::MellowDSP::Settings';
 }
 
 1;
